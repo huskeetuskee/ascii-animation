0632ba69a6e4c015bd97891e64294c8186cdda2c
huskeetuskee <huskeetuskee@gmail.com>
2025-08-31 16:50:24 +0300

    Add files via upload

---
+++ asciitry.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+#define GENISLIK 100
+#define UZUNLUK 33
+
+int main() {
+    FILE *txtfile = fopen("asci.txt", "r");
+    if (!txtfile) {
+        return 1;
+    }
+
+    char cizgi[GENISLIK + 2];
+    char kare[UZUNLUK][GENISLIK + 2];
+    while(1){
+
+    
+        for (int i=0; i<UZUNLUK; i++) {
+            if(!fgets(cizgi,sizeof(cizgi),txtfile)) {
+                fseek(txtfile, 0, SEEK_SET);
+                fgets(cizgi,sizeof(cizgi),txtfile);
+            }
+            strcpy(kare[i],cizgi);
+        }
+
+        printf("\e[1;1H\e[2J");
+
+        for(int x=0; x<UZUNLUK; x++){
+            printf("%s",kare[x]);
+        }
+        usleep(64000);
+    }
+        fclose(txtfile);
+        return 0;
+}
